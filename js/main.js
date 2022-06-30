a21e8faa9422f0cd29e8be45ce7d80c356c2f193
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-07-01 02:24:58 +0700

    Backend Final

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxKl3D2Fhxn7SVIIs8DMgqWFMHyT3v6h0F6B1N5PSl5Yt2hf_cUucG4944kWnEp58S0Jw/exec'
 const form = document.forms['seraboetan-page']
+const btnKirim = document.querySelector('.btn-kirim');
+      const btnLoading = document.querySelector('.btn-loading');
+      const myAlert = document.querySelector('.my-alert');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // ketika tombol submit diklik
+        // tampilkan tombol loading, hilangkan tombol kirim
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then((response) => {
+            // tampilkan tombol kirim, hilangkan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+            // tampilkan alert
+            myAlert.classList.toggle('d-none');
+            // reset formnya
+            form.reset();
+            console.log('Success!', response);
+          })
+          .catch((error) => console.error('Error!', error.message));
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message, 404))
-  })
+      });
