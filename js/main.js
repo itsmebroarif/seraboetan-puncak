594260cfc2e56706ba09d6607b18ccfbfc5f5563
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-07-01 02:24:10 +0700

    Ubah tampilan form dan backend

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxKl3D2Fhxn7SVIIs8DMgqWFMHyT3v6h0F6B1N5PSl5Yt2hf_cUucG4944kWnEp58S0Jw/exec'
-  const form = document.forms['namaForm']
+const form = document.forms['seraboetan-page']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
