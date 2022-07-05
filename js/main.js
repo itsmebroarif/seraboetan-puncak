77fb6fabda19e2927f5734c73b4ffe079045b1f8
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-07-05 11:59:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxKl3D2Fhxn7SVIIs8DMgqWFMHyT3v6h0F6B1N5PSl5Yt2hf_cUucG4944kWnEp58S0Jw/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxiDRozzRxwdXM21IctWpX0wcC9XeXIzhVHSQEnm2s1ElLwlxZhkccGf5LKNhECA01j_Q/exec'
 const form = document.forms['seraboetan-page']
 const btnKirim = document.querySelector('.btn-kirim');
-      const btnLoading = document.querySelector('.btn-loading');
-      const myAlert = document.querySelector('.my-alert');
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        // ketika tombol submit diklik
-        // tampilkan tombol loading, hilangkan tombol kirim
-        btnLoading.classList.toggle('d-none');
-        btnKirim.classList.toggle('d-none');
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-          .then((response) => {
-            // tampilkan tombol kirim, hilangkan tombol loading
-            btnLoading.classList.toggle('d-none');
-            btnKirim.classList.toggle('d-none');
-            // tampilkan alert
-            myAlert.classList.toggle('d-none');
-            // reset formnya
-            form.reset();
-            console.log('Success!', response);
-          })
-          .catch((error) => console.error('Error!', error.message));
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
 
-      });
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle(
+        Swal.fire({
+          icon: 'success',
+          title: 'Terima Kasih !',
+          text: 'Nantikan Trip Kami Selanjutnya!' 
+        })
+      );
+      // tampilkan alert
+      myAlert.classList.toggle('d-none');
+      // reset formnya
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => console.error('Error!', error.message));
+
+});
